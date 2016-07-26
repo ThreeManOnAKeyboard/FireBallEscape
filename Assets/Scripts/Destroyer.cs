682329238e0f7270671294d335d677c359beb2f8
ThreeManOnAKeyboard <threemanonakeyboard@gmail.com>
2016-07-27 00:58:02 +0300

    version 0.0.1
    tiksueshtili

--- Assets/Scripts/Destroyer.cs
+++ Assets/Scripts/Destroyer.cs
@@ -4,13 +4,6 @@ public class Destroyer : MonoBehaviour
 {
     public void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.transform.parent != null)
-        {
-            Destroy(col.transform.parent.gameObject);
-        }
-        else
-        {
-            Destroy(col.gameObject);
-        }
+		Destroy(col.gameObject);
     }
 }
