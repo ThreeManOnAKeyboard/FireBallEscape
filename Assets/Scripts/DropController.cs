0dfb9067141eaddb4dd04c4f6ee0e7203d5ee65b
NudeNULL <palchingleb@gmail.com>
2016-07-27 14:40:10 +0300

    Added fuel drop spawn
    Spawner enhancement

--- Assets/Scripts/DropController.cs
+++ Assets/Scripts/DropController.cs
@@ -2,13 +2,13 @@
 
 public class DropController : MonoBehaviour
 {
-    public Tags.tags playerTag;
+	public Tags.tags playerTag;
 
-    public void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.gameObject.tag == playerTag.ToString())
-        {
-            print("hero damaged");
-        }
-    }
+	public void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == playerTag.ToString())
+		{
+
+		}
+	}
 }
