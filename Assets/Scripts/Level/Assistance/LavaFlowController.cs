a9de9465c0ef82cd4bde674331de9caab68bf0da
Gleb <palchingleb@gmail.com>
2016-10-23 18:21:20 +0300

    0.8.2b
    Geyser are done.

--- Assets/Scripts/Level/Assistance/LavaFlowController.cs
+++ Assets/Scripts/Level/Assistance/LavaFlowController.cs
@@ -8,39 +8,43 @@ public class LavaFlowController : MonoBehaviour
 
 	private BoxCollider2D thisCollider2D;
 	private PlayerController playerController;
-	private ParticleSystem lavaParticleSystem;
+	private Transform meshTransform;
 
 	void Awake()
 	{
 		thisCollider2D = GetComponent<BoxCollider2D>();
 		playerController = FindObjectOfType<PlayerController>();
-		lavaParticleSystem = GetComponentInChildren<ParticleSystem>();
+		meshTransform = GetComponentInChildren<MeshFilter>().gameObject.transform;
 	}
 
 	// Use this for initialization
 	void OnEnable()
 	{
 		Vector3 position = transform.position;
-		Quaternion rotation = transform.rotation;
+		Vector3 localScale = transform.localScale;
 
+		// Set the Lava Flow gameobject on a random side of screen
 		if (Random.Range(0, 2) == 0)
 		{
 			// Right side
 			position.x = CameraController.rightBorder - borderOffset;
-			//rotation = Quaternion.Euler(Random.Range(0f, 359f), 0f, Random.Range(-angleRange, angleRange));
-			rotation = Quaternion.Euler(0f, 0f, rightBorderZAngle);
+			transform.rotation = Quaternion.Euler(0f, 0f, rightBorderZAngle);
+			localScale.x = 1f;
 		}
 		else
 		{
 			// Left side
 			position.x = CameraController.leftBorder + borderOffset;
-			//rotation = Quaternion.Euler(0f, 0f, 180f + Random.Range(-angleRange, angleRange));
-			rotation = Quaternion.Euler(0f, 0f, leftBorderZAngle);
+			transform.rotation = Quaternion.Euler(0f, 0f, leftBorderZAngle);
+			localScale.x = -1f;
 		}
 
 		// Asign values
 		transform.position = position;
-		transform.rotation = rotation;
+		transform.localScale = localScale;
+
+		// Change the rotation of x axis of mesh object so it looks different
+		meshTransform.Rotate(Vector3.right * Random.Range(0f, 359f));
 
 		// Enable the lava flow
 		thisCollider2D.enabled = true;
@@ -49,5 +53,13 @@ public class LavaFlowController : MonoBehaviour
 	void OnDisable()
 	{
 		thisCollider2D.enabled = false;
+	}
+
+	public void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == Tags.PLAYER && !playerController.isUnderSuperShield)
+		{
+			playerController.FullHeal();
+		}
 	}
 }
--- Assets/Scripts/Level/Obstacles/SteamGeyserController.cs
+++ Assets/Scripts/Level/Obstacles/SteamGeyserController.cs
@@ -3,56 +3,62 @@ using UnityEngine;
 
 public class SteamGeyserController : MonoBehaviour
 {
+	[Header("Configuration parameters")]
 	public float ejectDuration;
 	public float ejectCooldown;
 	public float angleRange = 40f;
 	public float offset;
 
+	[Header("Particle Systems")]
 	public ParticleSystem inactiveParticleSystem;
 	public ParticleSystem activeParticleSystem;
+
 	private BoxCollider2D thisCollider2D;
 	private PlayerController playerController;
+	private Transform meshTransform;
 
-	void Awake()
+	private void Awake()
 	{
 		thisCollider2D = GetComponent<BoxCollider2D>();
 		playerController = FindObjectOfType<PlayerController>();
+		meshTransform = GetComponentInChildren<MeshFilter>().gameObject.transform;
 	}
 
 	// Use this for initialization
-	void OnEnable()
+	private void OnEnable()
 	{
 		Vector3 position = transform.position;
-		Quaternion rotation = transform.rotation;
 
 		if (Random.Range(0, 2) == 0)
 		{
 			// Right side
 			position.x = CameraController.rightBorder - offset;
-			rotation = Quaternion.Euler(Random.Range(0f, 359f), 0f, Random.Range(-angleRange, angleRange));
+			transform.rotation = Quaternion.Euler(0f, 0f, Random.Range(-angleRange, angleRange));
 		}
 		else
 		{
 			// Left side
 			position.x = CameraController.leftBorder + offset;
-			rotation = Quaternion.Euler(0f, 0f, 180f + Random.Range(-angleRange, angleRange));
+			transform.rotation = Quaternion.Euler(0f, 0f, 180f + Random.Range(-angleRange, angleRange));
 		}
 
 		// Asign values
 		transform.position = position;
-		transform.rotation = rotation;
+
+		// Change the rotation of x axis of mesh object so it looks different
+		meshTransform.Rotate(Vector3.right * Random.Range(0f, 359f));
 
 		StartCoroutine(Eject());
 	}
 
-	void OnDisable()
+	private void OnDisable()
 	{
 		activeParticleSystem.Stop();
 		inactiveParticleSystem.Play();
 		thisCollider2D.enabled = false;
 	}
 
-	IEnumerator Eject()
+	private IEnumerator Eject()
 	{
 		while (true)
 		{
