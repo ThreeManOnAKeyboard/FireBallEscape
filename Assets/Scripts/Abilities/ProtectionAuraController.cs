4f9120b4de13e6c77f09cc8d2d70c05de280bf05
Gleb <palchingleb@gmail.com>
2016-11-12 17:22:31 +0200

    0.9.1h
    Dubina

--- Assets/Scripts/Abilities/ProtectionAuraController.cs
+++ Assets/Scripts/Abilities/ProtectionAuraController.cs
@@ -3,49 +3,32 @@ using UnityEngine;
 
 public class ProtectionAuraController : AbilityController
 {
-	//public GameObject collisionEffect;
-	//public float duration;
+	public float duration;
+	public float maxAngleRange;
 
-	//private int currentQueue = 3001;
+	private void Awake()
+	{
+		transform.parent = GameObject.FindWithTag(Tags.PLAYER).transform;
+		transform.localPosition = Vector3.zero;
+	}
 
-	//private void Awake()
-	//{
-	//	transform.parent = GameObject.FindWithTag(Tags.PLAYER).transform;
-	//	transform.localPosition = Vector3.zero;
-	//}
+	private void OnEnable()
+	{
+		StartCoroutine(Disable());
+	}
 
-	//private void OnEnable()
-	//{
-	//	StartCoroutine(Disable());
-	//}
+	private void Update()
+	{
+		//transform.rotation = Quaternion.Euler
+		//(
 
-	//private IEnumerator Disable()
-	//{
-	//	yield return new WaitForSecondsRealtime(duration);
+		//);
+	}
 
-	//	gameObject.SetActive(false);
-	//}
+	private IEnumerator Disable()
+	{
+		yield return new WaitForSeconds(duration);
 
-	//public void OnTriggerEnter2D(Collider2D col)
-	//{
-	//	GameObject effectInstance = ObjectPool.Instance.GetPooledObject(collisionEffect);
-	//	Transform effectTransform = effectInstance.transform;
-	//	effectTransform.parent = transform;
-	//	effectTransform.localScale = Vector3.one;
-	//	effectTransform.localPosition = Vector3.zero;
-	//	effectTransform.LookAt(Vector3.forward);
-	//	effectTransform.Rotate(Vector3.up * 270f);
-	//	effectInstance.GetComponent<Renderer>().material.renderQueue = currentQueue - 1000;
-	//	effectInstance.SetActive(true);
-
-	//	// Draw collision effect on top of other effects
-	//	if (currentQueue > 4000)
-	//	{
-	//		currentQueue = 3001;
-	//	}
-	//	else
-	//	{
-	//		++currentQueue;
-	//	}
-	//}
+		gameObject.SetActive(false);
+	}
 }
