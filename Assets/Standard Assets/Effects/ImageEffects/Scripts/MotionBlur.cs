34941d80d6d80c4a1a491056a698232ab10dfc04
Gleb <palchingleb@gmail.com>
2017-06-28 11:11:04 +0300

    0.12.0
    Holy shit deleted mother project, god bless Muldova.
    
    kill meh

--- Assets/Standard Assets/Effects/ImageEffects/Scripts/MotionBlur.cs
+++ Assets/Standard Assets/Effects/ImageEffects/Scripts/MotionBlur.cs
@@ -8,59 +8,64 @@ using UnityEngine;
 // texture.
 namespace UnityStandardAssets.ImageEffects
 {
-	[ExecuteInEditMode]
-	[AddComponentMenu("Image Effects/Blur/Motion Blur (Color Accumulation)")]
-	[RequireComponent(typeof(Camera))]
-	public class MotionBlur : ImageEffectBase
-	{
-		[Range(0.0f, 0.92f)]
-		public float blurAmount = 0.8f;
-		public bool extraBlur = false;
+    [ExecuteInEditMode]
+    [AddComponentMenu("Image Effects/Blur/Motion Blur (Color Accumulation)")]
+    [RequireComponent(typeof(Camera))]
+    public class MotionBlur : ImageEffectBase
+    {
+        [Range(0.0f, 0.92f)]
+        public float blurAmount = 0.8f;
+        public bool extraBlur = false;
 
-		private RenderTexture accumTexture;
+        private RenderTexture accumTexture;
 
-		override protected void OnDisable()
-		{
-			base.OnDisable();
-			DestroyImmediate(accumTexture);
-		}
+        override protected void Start()
+        {
+            base.Start();
+        }
 
-		// Called by camera to apply image effect
-		void OnRenderImage(RenderTexture source, RenderTexture destination)
-		{
-			// Create the accumulation texture
-			if (accumTexture == null || accumTexture.width != source.width || accumTexture.height != source.height)
-			{
-				DestroyImmediate(accumTexture);
-				accumTexture = new RenderTexture(source.width, source.height, 0);
-				accumTexture.hideFlags = HideFlags.HideAndDontSave;
-				Graphics.Blit(source, accumTexture);
-			}
+        override protected void OnDisable()
+        {
+            base.OnDisable();
+            DestroyImmediate(accumTexture);
+        }
 
-			// If Extra Blur is selected, downscale the texture to 4x4 smaller resolution.
-			if (extraBlur)
-			{
-				RenderTexture blurbuffer = RenderTexture.GetTemporary(source.width / 4, source.height / 4, 0);
-				accumTexture.MarkRestoreExpected();
-				Graphics.Blit(accumTexture, blurbuffer);
-				Graphics.Blit(blurbuffer, accumTexture);
-				RenderTexture.ReleaseTemporary(blurbuffer);
-			}
+        // Called by camera to apply image effect
+        void OnRenderImage (RenderTexture source, RenderTexture destination)
+        {
+            // Create the accumulation texture
+            if (accumTexture == null || accumTexture.width != source.width || accumTexture.height != source.height)
+            {
+                DestroyImmediate(accumTexture);
+                accumTexture = new RenderTexture(source.width, source.height, 0);
+                accumTexture.hideFlags = HideFlags.HideAndDontSave;
+                Graphics.Blit( source, accumTexture );
+            }
 
-			// Clamp the motion blur variable, so it can never leave permanent trails in the image
-			blurAmount = Mathf.Clamp(blurAmount, 0.0f, 0.92f);
+            // If Extra Blur is selected, downscale the texture to 4x4 smaller resolution.
+            if (extraBlur)
+            {
+                RenderTexture blurbuffer = RenderTexture.GetTemporary(source.width/4, source.height/4, 0);
+                accumTexture.MarkRestoreExpected();
+                Graphics.Blit(accumTexture, blurbuffer);
+                Graphics.Blit(blurbuffer,accumTexture);
+                RenderTexture.ReleaseTemporary(blurbuffer);
+            }
 
-			// Setup the texture and floating point values in the shader
-			material.SetTexture("_MainTex", accumTexture);
-			material.SetFloat("_AccumOrig", 1.0F - blurAmount);
+            // Clamp the motion blur variable, so it can never leave permanent trails in the image
+            blurAmount = Mathf.Clamp( blurAmount, 0.0f, 0.92f );
 
-			// We are accumulating motion over frames without clear/discard
-			// by design, so silence any performance warnings from Unity
-			accumTexture.MarkRestoreExpected();
+            // Setup the texture and floating point values in the shader
+            material.SetTexture("_MainTex", accumTexture);
+            material.SetFloat("_AccumOrig", 1.0F-blurAmount);
 
-			// Render the image using the motion blur shader
-			Graphics.Blit(source, accumTexture, material);
-			Graphics.Blit(accumTexture, destination);
-		}
-	}
+            // We are accumulating motion over frames without clear/discard
+            // by design, so silence any performance warnings from Unity
+            accumTexture.MarkRestoreExpected();
+
+            // Render the image using the motion blur shader
+            Graphics.Blit (source, accumTexture, material);
+            Graphics.Blit (accumTexture, destination);
+        }
+    }
 }
